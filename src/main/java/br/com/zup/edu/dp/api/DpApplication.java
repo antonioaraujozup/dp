f147a4b41a941e9103255ae6e3a122bef473d2ce
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-07 14:15:46 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/dp/api/DpApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.dp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpApplication.class, args);
+	}
+
+}
